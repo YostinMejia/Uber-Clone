4f9a004ec2633814e49f73535df62eb370c34d7e
Yostin <110141509+YostinMejia@users.noreply.github.com>
2024-02-09 17:58:40 -0500

    inicio y llegada

--- public/map/index.js
+++ public/map/index.js
@@ -1,43 +1,16 @@
-import { geocoder } from "./geocoder.js";
-import { createMap } from "./map.js";
-import { Travel } from "./osrm.js";
-const map = createMap()
-geocoder(map)
+import { Geocoder } from "./geocoder.js";
+import { Travel } from "./travel.js"
+import { Map } from "./map.js"
+import { Markers } from "./marker.js";
+import { googleLayers, defaultLayer } from "./layers.js";
 
-var markersList = [];
-
-// Agregar un controlador de eventos de clic al mapa
-map.on('click', async function (e) {
-    // Obtener las coordenadas de donde se hizo clic
-    var lat = e.latlng.lat;
-    var lng = e.latlng.lng;
-
-    // Agregar un marcador en la ubicación donde se hizo clic
-    // Solo se acepta marcar rutas entre dos puntos
-    if (markersList.length < 2) {
-
-        let marker = L.marker([lat, lng], {
-            draggable: true
-        }).addTo(map)
-            .bindPopup('Latitud: ' + lat + '<br>Longitud: ' + lng)
-            .openPopup()
-
-        markersList.push(marker);
+const markers = new Markers()
+const travel = new Travel("car")
+const map = new Map(googleLayers, defaultLayer, travel, markers)
+await map.createMap()
+const geocoder = new Geocoder()
+geocoder.initGeocoder(map, travel, markers)
 
 
-        // Agregar controlador de eventos 'moveend' al marcador
-        marker.on("moveend", async function (event) {
-            // var newLatLng = event.latlng; 
-            if (markersList.length >= 2) {
-                await Travel.travel(markersList, map, "car")
-            }
 
-        });
 
-    }
-
-    if (markersList.length == 2) {
-        await Travel.travel(markersList, map, "car")
-    }
-
-});
---
+++ public/map/layers.js
@@ -0,0 +1,17 @@
+// // layers
+const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+
+const googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
+
+export const googleLayers = {
+    "Street": googleStreets,
+    "Hybrid": googleHybrid
+};
+
+export const defaultLayer = googleStreets
